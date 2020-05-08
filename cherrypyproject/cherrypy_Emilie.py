45ec751dd994c922bbff87df506a68f395a1cbfb
EmilieSpriet <65027844+EmilieSpriet@users.noreply.github.com>
2020-05-08 17:39:12 +0200

    Update and rename pages_web.py to cherrypy_Emilie.py

---
+++ cherrypyproject/cherrypy_Emilie.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import cherrypy
+import os
+
+
+class site(object):
+ def __init__(self):
+  self.produits = Produits()
+  self.recapitulatif = Recapitulatif()
+
+class Produits(object):
+ @cherrypy.expose
+ def __init__(self):
+    pass
+  
+ def index(self):
+   return open("html/produits.html")
+ index.exposed = True
+ 
+   @cherrypy.expose
+   def Recapitulatif(self, quantitePates, quantiteRiz, quantiteSucre, quantiteFarine):
+        some_string = quantitePates + ' ' + quantiteRiz + ' ' + quantiteSucre + ' ' + quantiteFarine
+        cherrypy.session['mystring'] = some_string
+        file = open("test.txt","w")
+        file.write(some_string)
+        file.close()
+        return some_string
+       
+   def display(self):
+     return cherrypy.session['mystring']
+    
+    
+class Recapitulatif(object):
+    @cherrypy.expose
+    def index(self):
+        return open("html/recapitulatif.html")
+    index.exposed = True
+    
+    # @cherrypy.expose
+    # def Recapitulatif(self, quantitePates, quantiteRiz, quantiteSucre, quantiteFarine):
+        # some_string = quantitePates + ' ' + quantiteRiz + ' ' + quantiteSucre + ' ' + quantiteFarine
+        # cherrypy.session['mystring'] = some_string
+        # file = open("test.txt","w")
+        # file.write(some_string)
+        # file.close()
+        # return some_string
+ 
+    # @cherrypy.expose
+    # def demarrer(self, quantitePate):
+        # return recap.format(quantitePate)
+ 
+configcss = {
+        '/css':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir':os.path.abspath("./css")
+        },
+        '/':
+        {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        }   
+        }
+ 
+cherrypy.quickstart(site(), config = configcss)
