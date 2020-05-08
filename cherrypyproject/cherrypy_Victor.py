9f1c3d083550bec29f1e9e10637adf62205cbe6c
Victor CANAL <canal.victor@yahoo.fr>
2020-05-08 14:09:44 +0200

    test

--- cherrypyproject/cherrypy_Victor.py
+++ cherrypyproject/cherrypy_Victor.py
@@ -2,6 +2,8 @@
 
 import cherrypy
 import os
+import random
+import string
 
 class Acceuil(object):
     @cherrypy.expose 
@@ -14,14 +16,29 @@ class Acceuil(object):
     index.exposed = True
 
 class Inscription(object):
+    @cherrypy.expose 
     def __init__(self):
         #self.acceuil = Acceuil() #censé être pour les navigations bars
-        # self.connexion = Connexion()
+        #self.connexion = Connexion()
         pass
         
     def index(self):
         return open("html/inscription.html")
     index.exposed = True
+    
+    @cherrypy.expose
+    def generate(self, inputidentifiant, inputpassword, inputnom, inputprenom, inputadresse, inputemail, inputnumero):
+        # some_string = ''.join(random.sample(string.hexdigits, int(length)))
+        # cherrypy.session['mystring'] = some_string
+        some_string = inputidentifiant + inputpassword + inputnom + inputprenom + inputadresse + inputemail + inputnumero
+        cherrypy.session['mystring'] = some_string
+        file = open("test.txt","w")
+        file.write(some_string)
+        file.close()
+        return some_string
+
+    def display(self):
+        return cherrypy.session['mystring']
     
 class Connexion(object):
     def __init__(self):
@@ -37,6 +54,11 @@ configcss = {
         '/css':
         { 'tools.staticdir.on':True,
           'tools.staticdir.dir':os.path.abspath("./css")
+        },
+        '/':
+        {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
         }
         #Pas besoin du code en dessous pour l'instant
         #,
@@ -48,5 +70,5 @@ configcss = {
         # { 'tools.staticfile.on':True,
         #   'tools.staticfile.filename':os.path.abspath("./css/acceuil.css")
         # }
-    }
+        }
 cherrypy.quickstart(Acceuil(), config = configcss)
